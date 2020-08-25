d22fac49a319de38b943cc9fbd94f0d51dd69c16
Stéphane Lesimple <speed47_github@speed47.net>
2020-08-25 20:15:36 +0200

    add regtest/runtests.sh

---
+++ regtest/runtests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+allfailed=0
+for i in rs*.bash; do
+   ( DVDISASTER_SCSI_SIMULATED_NODELAY=1 ./$i ); ret=$?
+   allfailed=$((allfailed + ret))
+done
+echo Failed $allfailed tests
+exit $allfailed
