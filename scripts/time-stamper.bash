22b2aba490425080e817d91543dec5f6c8913eec
Stéphane Lesimple <speed47_github@speed47.net>
2020-08-31 22:11:20 +0200

    fix: windows: flavor in build.h was always UNKNOWN

--- scripts/time-stamper.bash
+++ scripts/time-stamper.bash
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 build=$(grep BUILDNUM $1 2>/dev/null | cut -d\  -f3)
 build=$((build+1))
-flavor=$(grep FLAVOR $1 2>/dev/null | cut -d\  -f3)
-if [ -z "$flavor" ]; then
-   flavor=UNKNOWN
-fi
-echo "#define FLAVOR $flavor" >$1
+cat >$1 <<EOF
+#ifdef CLI
+#define FLAVOR CLI
+#else
+#define FLAVOR GUI
+#endif
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+EOF
 echo "#define BUILDNUM $build" >>$1
 if git describe >/dev/null 2>/dev/null; then
    gitver=$(git describe --tags --dirty)
-   echo "#define BUILD \"$gitver-$flavor-speed47.build$build\"" >>$1
+   echo "#define BUILD \"$gitver-\" TOSTRING(FLAVOR) \"-speed47.build$build\"" >>$1
 else
-   echo "#define BUILD \"$flavor-speed47.build$build\"" >>$1
+   echo "#define BUILD TOSTRING(FLAVOR) \"-speed47.build$build\"" >>$1
 fi
 date=$(date +"%d.%m.%y (%A, %H:%M)")
 echo "#define BDATE \"$date\"" >>$1
