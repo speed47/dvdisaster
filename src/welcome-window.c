36fa2db965bc961022b1b15bcc82beb80a26dbe4
Stéphane Lesimple <speed47_github@speed47.net>
2021-02-22 21:58:09 +0100

    release pl8

--- src/welcome-window.c
+++ src/welcome-window.c
@@ -79,7 +79,7 @@ static gboolean expose_cb(GtkWidget *widget, GdkEventExpose *event, gpointer dat
 	 Closure->invisibleDash = g_strdup_printf("<span color=\"#%02x%02x%02x\">-</span>",
 						  bg->red>>8, bg->green>>8, bg->blue>>8);
 
-	 AboutText(box, _("This is <b>v0.79.6-pl7</b>. The patchlevel series are enhanced from the last upstream pre-release.\n"
+	 AboutText(box, _("This is <b>v0.79.6-pl8</b>. The patchlevel series are enhanced from the last upstream pre-release.\n"
 			  "We add support for a CLI version, BD-R TL/QL, a Windows build, an option to produce bigger BD-R RS03,\n"
 			  "images, support for stripping ECC from ISOs, re-enabled adaptive reading (except for RS03), and more.\n"
 			  "\n"
