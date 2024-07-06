4b022234942802a2fd57d239693c86f40ae1dc48
Stéphane Lesimple <speed47_github@speed47.net>
2024-07-06 19:19:49 +0200

    fix: prevent AppImage from reading hosts' immodules.cache file

--- contrib/AppRun.sh
+++ contrib/AppRun.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DIR="$(readlink -f "$(dirname "$0")")"
 export GTK_PATH="$DIR/usr/lib/gtk-2.0"
+export GTK_IM_MODULE_FILE=/dev/null
 export DVDISASTER_DOCDIR="$DIR/usr/share/doc/dvdisaster"
 exec "$DIR/usr/bin/dvdisaster" "$@"
